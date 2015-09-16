418f033e01ceac25d39a886ea883172f59195c32
Ruot Thabac Duany <rtduany@gmail.com>
2015-09-16 09:44:30 +0300

    added Fibonacci.java file

---
+++ Fibonacci.java
@@ -0,0 +1,18 @@
+// Using java, write a program that prints out the numbers in the Fibonacci series
+// between 1 and 50 i.e.
+// The first ten Fibonacci numbers are:
+// ```
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+// ```
+// The logic behind is that each subsequent number is the sum of the previous two i.e
+// 0,1 are the first two, then 1,1 are the next then 1,2 follows e.t.c
+
+//define a Fibonacci class
+public class Fibonacci
+{
+    //lets define our main method
+    public static void main(String[] args)
+    {
+        
+    }
+}
