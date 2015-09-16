5fa3449c1e607d6b141e3dcd8d2d4d4d16576e00
Ruot Thabac Duany <rtduany@gmail.com>
2015-09-16 11:16:42 +0300

    added code to loop to finish problem

--- Fibonacci.java
+++ Fibonacci.java
@@ -7,6 +7,8 @@
 // The logic behind is that each subsequent number is the sum of the previous two i.e
 // 0,1 are the first two, then 1,1 are the next then 1,2 follows e.t.c
 
+import java.io.*;
+import java.util.*;
 //define a Fibonacci class
 public class Fibonacci
 {
@@ -23,9 +25,13 @@ public class Fibonacci
         //the for statement will start with the second number in the sequence
         System.out.println(f0);
         //lets define a for loop to iterate through the recursive formula
-        for(int x = 0; x <= 50; x++){
+        for(int x = 1; x <= 49; x++){
             f2 = f1 + f0; //f2 = 1 + 1
-            System.out.println(f2)
+            System.out.println(f2);
+            //f0 will equal f1 due to the fact the Fib Seq adds the previous number with the current
+            f0 = f1;
+            //f1 will equal the new fib number so it can be repeated
+            f1 = f2;
         }
     }
 }
