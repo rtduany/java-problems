652495c5fd04a33cbeecaf95ab9f7d841f0e9a0c
rtduany <rtduany@gmail.com>
2015-09-23 01:23:59 +0300

    added variables to the Josephus.java class

--- Josephus.java
+++ Josephus.java
@@ -1,4 +1,10 @@
 package Flavious;
 //define a Josephus class
 class Josephus {
+	//define our variables
+	private int numOfPeople; // number of people in a circle
+    private int countNum; // number used for counting off
+    private Node head;
+    private Node tail;
+    CircularList circle;
 }
