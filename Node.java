f60d91332c627f2c97b29dc04929d91287094548
rtduany <rtduany@gmail.com>
2015-09-23 00:17:33 +0300

    added a constructor and a displayNode class to Node.java

--- Node.java
+++ Node.java
@@ -4,4 +4,13 @@ class Node {
 	//lets declare our variables
     int iData;
     Node next;
+
+	public Node(int x) {
+        iData = x;
+    }
+	//define a displayNode method
+    public void displayNode() {
+        System.out.print(iData + " ");
+
+    }
 }
