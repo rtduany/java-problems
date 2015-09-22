1302a9ade42115344a814ae9cccb82d9f43b15ec
rtduany <rtduany@gmail.com>
2015-09-23 00:33:59 +0300

    added variables for CircularList.java class

--- CircularList.java
+++ CircularList.java
@@ -1,4 +1,9 @@
 package Flavious;
 //define a CircularList class
 class CircularList {
+	//lets define our variables
+	private Node first;
+    private Node last;
+    private Node current;
+    private int count; // total items in the list
 }
